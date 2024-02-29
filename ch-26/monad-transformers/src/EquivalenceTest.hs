22b6b800bdc8c2ef6df54d70ea6ef354e0cbade9
ja-cha <jan_abt@yahoo.com>
2024-02-29 10:21:08 +0700

    provided additional commentary

--- ch-26/monad-transformers/src/EquivalenceTest.hs
+++ ch-26/monad-transformers/src/EquivalenceTest.hs
@@ -6,34 +6,48 @@ import Control.Monad.Reader
 import Control.Monad.Trans.Maybe
 
 {-
-    ReaderT r Maybe and MaybeT (Reader r) are not exactly equivalent,
-    but closely related and can often be used interchangeably depending on the context. 
-    These types involve combining the Reader and Maybe monads in different orders.
+    "ReaderT r Maybe a" and "MaybeT (Reader r) a" are not exactly equivalent.
+    They are often be used interchangeably depending on the context. 
 
-    ReaderT r Maybe a: 
+    In essence, the difference lies in the order of stacking the monad transformers: 
+     * readerTMaybeExample
+        a "Maybe String" monad is encapsulated within a "ReaderT Int" monad transformer.
+     * maybeTReaderExample
+          a "Reader Int" monad is encapsulated within a "MaybeT String" monad transformer, 
+
+
+    "ReaderT r Maybe a": 
     * Represents a computation that has access to a shared environment r. 
     * May return a Maybe a result.
 
-    MaybeT (Reader r) a: 
+    "MaybeT (Reader r) a": 
     * Represents a computation in the environment r that may fail.
     * May return a Maybe (Reader r a) result.  
+
+    Recap:  ReaderT monad transformer 
+      * designed to carry an environment throughout the computation.
+      * the "ask" function is used within the context of the 
+        "ReaderT Int Maybe" monad transformer stack. 
+      * here, ask is providing access to the 
+        environment value of type "Int" within the "ReaderT" monad. 
+
 -}
 
 -- ReaderT r Maybe a
 readerTMaybeExample :: ReaderT Int Maybe String
-readerTMaybeExample = do
-  env <- ask
-  if env > 0
-    then lift $ return "Positive"
-    else lift $ return "Not Positive"
+readerTMaybeExample =
+  ask >>= \env ->
+    if env > 0
+      then lift $ return "Positive"
+      else lift $ return "Not Positive"
 
 -- MaybeT (Reader r) a
 maybeTReaderExample :: MaybeT (Reader Int) String
-maybeTReaderExample = do
-  env <- lift ask
-  if env > 0
-    then return "Positive"
-    else return "Not Positive"
+maybeTReaderExample =
+   ask >>= \env ->
+    if env > 0
+      then return "Positive"
+      else return "Not Positive"
 
 -- ============================== RUN EXAMPLES ==============================
 
