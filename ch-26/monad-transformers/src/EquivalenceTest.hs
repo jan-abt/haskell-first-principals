5302b392b64ec2d46570cb3a58e896c009bcc253
ja-cha <jan_abt@yahoo.com>
2024-02-29 09:22:52 +0700

    renamed function

--- ch-26/monad-transformers/src/EquivalenceTest.hs
+++ ch-26/monad-transformers/src/EquivalenceTest.hs
@@ -19,38 +19,34 @@ import Control.Monad.Trans.Maybe
     * May return a Maybe (Reader r a) result.  
 -}
 
-
-
-
 -- ReaderT r Maybe a
-readerMaybeExample :: ReaderT Int Maybe String
-readerMaybeExample = do
+readerTMaybeExample :: ReaderT Int Maybe String
+readerTMaybeExample = do
   env <- ask
   if env > 0
     then lift $ return "Positive"
     else lift $ return "Not Positive"
 
 -- MaybeT (Reader r) a
-maybeReaderExample :: MaybeT (Reader Int) String
-maybeReaderExample = do
+maybeTReaderExample :: MaybeT (Reader Int) String
+maybeTReaderExample = do
   env <- lift ask
   if env > 0
     then return "Positive"
     else return "Not Positive"
 
-
 -- ============================== RUN EXAMPLES ==============================
 
 -- Example function using ReaderT r Maybe
--- runReaderMaybeExample 0
--- runReaderMaybeExample 1
-runReaderMaybeExample :: Int -> Maybe String
-runReaderMaybeExample env = 
-  runReaderT readerMaybeExample env
+-- runReaderTMaybeExample 0
+-- runReaderTMaybeExample 1
+runReaderTMaybeExample :: Int -> Maybe String
+runReaderTMaybeExample env = 
+  runReaderT readerTMaybeExample env
 
 -- Example function using MaybeT (Reader r)
--- runMaybeReaderExample 0
--- runMaybeReaderExample 1
-runMaybeReaderExample :: Int -> Maybe String
-runMaybeReaderExample env = 
-  runReader (runMaybeT maybeReaderExample) env
+-- runMaybeTReaderExample 0
+-- runMaybeTReaderExample 1
+runMaybeTReaderExample :: Int -> Maybe String
+runMaybeTReaderExample env = 
+  runReader (runMaybeT maybeTReaderExample) env
